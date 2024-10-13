9945dfb5fac7d95491bb6e3cf6bb20462cd2a2e8
Patrick Flanagan <G00431255@atu.ie>
2024-10-13 13:35:35 +0100

    fixed code but without array method

--- src/main/java/StudentApp.java
+++ src/main/java/StudentApp.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class StudentApp {
     public static void main(String[] args) {
-        //prompt user for details
+//prompt user for details
 
         System.out.println("Please enter your name ");
         Scanner sc= new Scanner(System.in);
@@ -13,43 +13,31 @@ public class StudentApp {
         String studentCourse=sc.nextLine();
 
 
-        //first Student
+//first Student
         Student firstStudent =new Student();
+        firstStudent.setName(studentName);
         firstStudent.setName(studentName);//this one is fine
         firstStudent.setEmail(studentEmail);
         firstStudent.setCourse(studentCourse);
 
 
 
-        //second Student
-        System.out.println("Please enter Second Student name ");
+//second Student
 
-        String studentName2= sc.nextLine();
-        System.out.println("Please enter your email");
-        String studentEmail2= sc.nextLine();        //change theese two 
-        System.out.println("Please enter your course");
+        System.out.println("Please enter Second Student email");
+        String studentEmail2= sc.nextLine();
+        System.out.println("Please enter Second Student course");
         String studentCourse2=sc.nextLine();
 
-        Student secondStudent =new Student();
-        secondStudent.setName(studentName2);
+        Student secondStudent =new Student("Shane");
         secondStudent.setEmail(studentEmail2);
         secondStudent.setCourse(studentCourse2);
 
 
 
-        //third Student
-        System.out.println("Please enter third Student name ");
-        String studentName3= sc.nextLine();
-        System.out.println("Please enter your email");
-        String studentEmail3= sc.nextLine();
-        System.out.println("Please enter your course");
-        String studentCourse3=sc.nextLine();
+//third Student
 
-        Student thirdStudent =new Student();
-        thirdStudent.setName(studentName3);
-        thirdStudent.setEmail(studentEmail3);
-        thirdStudent.setCourse(studentCourse3);
-
+        Student thirdStudent =new Student("David" ,"David@atu.ie","Sport Science");
 
         System.out.println( firstStudent.toString());
         System.out.println( secondStudent.toString());
